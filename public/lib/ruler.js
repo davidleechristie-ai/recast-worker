dfd832b38d6d4fdd12bc951d7463193b222d9ceb
davidleechristie-ai <davidleechristie@yahoo.co.uk>
2026-08-29 17:08:38 +0100

    build v23

--- public/lib/ruler.js
+++
@@ -1,8 +0,0 @@
-const ruler = document.getElementById('ruler');
-if (ruler) {
-  for (let i = 0; i < 60; i++) {
-    const s = document.createElement('span');
-    s.textContent = i % 5 === 0 ? i : '';
-    ruler.appendChild(s);
-  }
-}
