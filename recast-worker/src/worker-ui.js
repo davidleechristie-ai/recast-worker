0ceb0e1a947da3cb8796018f985ad58ed147ac72
davidleechristie-ai <davidleechristie@yahoo.co.uk>
2026-09-02 17:22:07 +0100

    Fix broken logo assets and responsive header overflow

--- recast-worker/src/worker-ui.js
+++ recast-worker/src/worker-ui.js
@@ -1,7 +1,7 @@
 import release4Worker from './worker-release4.js';
 
-const UI_STYLE = '<link rel="icon" type="image/png" sizes="64x64" href="/assets/brand/recast-favicon-64.png"><link rel="apple-touch-icon" href="/icons/icon-192.png"><link rel="stylesheet" href="/ui-consistency.css?v=3">';
-const UI_SCRIPT = '<script src="/ui-consistency.js?v=3" defer></script>';
+const UI_STYLE = '<link rel="icon" type="image/png" sizes="64x64" href="/assets/brand/recast-favicon-64.png"><link rel="apple-touch-icon" href="/icons/icon-192.png"><link rel="stylesheet" href="/ui-consistency.css?v=4">';
+const UI_SCRIPT = '<script src="/ui-consistency.js?v=4" defer></script>';
 
 function applyUi(response) {
   const type = response.headers.get('content-type') || '';
